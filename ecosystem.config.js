8c60086ee7c8d6a9100e2969aef026fb8742e922
Aurélie VIOLETTE <orwel.violette@gmail.com>
2017-05-10 20:05:16 +0200

    Add deployement tools

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'mtg-league-api',
+      script    : 'server/server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'orwel',
+      host : 'vps',
+      ref  : 'origin/master',
+      repo : 'https://github.com/AurelieV/mtg-league.git',
+      path : '/home/orwel/mtg-league',
+      'post-deploy' : 'yarn install && npm build'
+    }
+  }
+};
